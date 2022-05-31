9028895f4110feb8c60a3662b2d0b6b70f5a7d73
Yunus Genç <52261695+yunusgnc@users.noreply.github.com>
2022-05-31 21:26:50 +0000

    fgvhjkl

--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,15 @@
 const { channel } = require("diagnostics_channel");
 const Discord = require("discord.js");
 var weather = require("weather-js");
-const file = "words.txt";
+const file = "./src/words.txt";
 const fs = require("fs");
 const { time } = require("console");
 const allIntents = new Discord.Intents(32767);
 const client = new Discord.Client({ intents: allIntents });
-const { MessageActionRow, MessageSelectMenu } = require('discord.js');
+const { MessageActionRow, MessageSelectMenu,MessageEmbed } = require('discord.js');
 const Database = require("@replit/database");
 const db = new Database();
-const TOKEN = process.env['TOKEN']
+const mySecret = process.env['TOKEN']
 var stringSimilarity = require("string-similarity");
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -44,12 +44,20 @@ client.on('interactionCreate', interaction => {
 	if (!interaction.isSelectMenu()) return;
   if(interaction.customId=="bolum"){
     var box = {'bolum':interaction.values[0]};
-interaction.message.channel.send("Adınızı ve Soyadınızı Giriniz").then(() => {
-   interaction.message.channel.awaitMessages(filter, {
+    const msg_filter = (m) => m.author.id === interaction.user.id;
+  console.log(interaction);
+  interaction.message.channel.send("Adınızı ve Soyadınızı Giriniz")
+    .then(() => {interaction.message.channel.awaitMessages({
+           filter: msg_filter,
           max: 1,
           time: 30000,
           errors: ['time']
-        });
+        })
+    .then((collected)=>{
+      console.log(collected.first().content)
+    })
+});
+  }
 });
 
 client.on("message", (msg) => {
@@ -170,73 +178,4 @@ function helpForUser(channel) {
   channel.send({ embeds: [exampleEmbed] });
 }
 
-// function sSSForUser(channel) {
-//   const exampleEmbed = new MessageEmbed()
-//     .setColor("#0099ff")
-//     .setTitle("Başlıkların altıntaki mesajı yazarak sonuca ulaşabilirsiniz.")
-//     .addFields(
-//       {
-//         name: "Her yıl programlara yeni kayıt yaptıracak öğrencileri kim belirliyor?  ",
-//         value: "sss1",
-//       },
-//       { name: "Üniversitemize kayıt nasıl yapılır?", value: "sss2" },
-//       {
-//         name: "Üniversiteye yeni kaydımı süresi içinde yaptıramazsam ne olur?",
-//         value: "sss3",
-//       },
-//       {
-//         name: "Danışmanım kimdir? Nasıl öğrenebilirim? Görevi nedir? ",
-//         value: "sss4",
-//       },
-//       {
-//         name: "Yeni kayıt işlemlerini şahsen yapmak zorunda mıyım?",
-//         value: "sss5",
-//       },
-//       {
-//         name: "Yeni kayıtta, ÖSYS Kılavuzunda istenen belgelerden başka belge istenir mi?",
-//         value: "sss6",
-//       },
-//       {
-//         name: "Üniversiteye yeni kayıt yaptırdıktan sonra başka işlem yapmam gerekiyor mu?",
-//         value: "sss7",
-//       },
-//       { name: "7-Ders başlama tarihlerini nasıl öğrenebilirim?", value: "sss8" }
-//     )
-//     .setTimestamp()
-//     .setFooter({
-//       text: "Aubamyng_17 : ",
-//       iconURL: "https://i.imgur.com/AfFp7pu.png",
-//     });
-
-//   channel.send({ embeds: [exampleEmbed] });
-// }
-
-function sssİnfo() {
-  if (msg.content == "sss1") {
-    msg.channel.send(sss.sss1);
-  }
-  if (msg.content == "sss2") {
-    msg.channel.send(sss.sss2);
-  }
-
-  if (msg.content == "sss3") {
-    msg.channel.send(sss.sss3);
-  }
-  if (msg.content == "sss4") {
-    msg.channel.send(sss.sss4);
-  }
-  if (msg.content == "sss5") {
-    msg.channel.send(sss.sss5);
-  }
-  if (msg.content == "sss6") {
-    msg.channel.send(sss.sss6);
-  }
-  if (msg.content == "sss7") {
-    msg.channel.send(sss.sss7);
-  }
-  if (msg.content == "sss8") {
-    msg.channel.send(sss.sss8);
-  }
-}
-
-client.login(TOKEN);
+client.login(mySecret);
